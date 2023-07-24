a8793e1fec6228d6f4ea744506d4dd5fc1bbdc4d
bergamota066666 <140364605+bergamota066666@users.noreply.github.com>
2023-07-24 00:22:05 -0300

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,27 @@
+package felipe;
+
+public class dowhile {
+
+	public static void main(String[] args) {
+
+		
+		int i = 1;
+		int max = 10;
+		
+		
+		while(i < max) {
+			 System.out.println("valor de i :" + i);
+			 i++;
+		}
+		
+		System.out.println(i);
+		
+		do {
+			i++;
+			System.out.println(i);
+		}while(i < 5) ; 
+			
+		System.out.println(i);
+		}
+		
+	}
